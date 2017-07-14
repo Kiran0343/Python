825b9df103a1820d7c1640b62c73b7f0be5fb0d2
kiran0343 <kiran.kamcharla92@gmail.com>
2017-07-13 20:34:22 -0500

    added comments

--- Assignment 1/even_odd.py
+++ Assignment 1/even_odd.py
@@ -6,11 +6,9 @@ program to check if a number is even or odd.
 # read value from the user
 number = input("enter a number to check : ")
 # checking if the number is odd or even
-try:
-    if number%2 == 0:
-        print str(number) + " is a an even number"
-    else:
-        print str(number) + " is a an odd number"
-# Throwing an exception if the user enters string
-except:
-    print "number can't be a string"
+
+if number%2 == 0:
+    print str(number) + " is a an even number"
+else:
+    print str(number) + " is a an odd number"
+
--- Assignment 1/leap_year.py
+++ Assignment 1/leap_year.py
@@ -5,12 +5,9 @@ program to check leap year.
 
 # reading the year number from the user
 year = input("Enter the year to check : ")
-try:
-    # Dividing the year by 4 to check if it is a leap year
-    if year % 4 == 0:
-        print str(year) + " is a leap year"
-    else:
-        print str(year) + " is not a leap year"
-# throwing an exception if the user enters string as year
-except:
-    print "year can't be a string"
+
+# Dividing the year by 4 to check if it is a leap year
+if year % 4 == 0:
+    print str(year) + " is a leap year"
+else:
+    print str(year) + " is not a leap year"
