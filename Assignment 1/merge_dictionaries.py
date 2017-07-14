d3c47504366bb127893f5ed7cebfc23d07323619
kiran0343 <kiran.kamcharla92@gmail.com>
2017-07-13 19:41:04 -0500

    added comments

--- Assignment 1/merge_dictionaries.py
+++ Assignment 1/merge_dictionaries.py
@@ -6,8 +6,12 @@ Python script to merge two Python dictionaries.
 # creating 2 sample dictionaries
 dict1 = {'Name' : 'kiran','City' : 'Dallas'}
 dict2 = {'course':'Python','Assignment_no' : 1}
+# printing the dict after merging
+print "Dictionary before merging : "
+print "dict1 : ",dict1
+print "dict1 : ",dict2
 # merging dict2 into dict1
 for key,value in dict2.items():
     dict1[key] = value
 # printing the dict after merging
-print dict1
+print "Dictionary after merging : ", dict1
